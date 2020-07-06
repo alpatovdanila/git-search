740cbb8872376100fd15c06d154b181d9f8d4665
alpatovdanila <alpatovdanila@gmail.com>
2020-07-06 10:05:31 +0300

    Updates

---
+++ precache-manifest.b09874c26d7abb7b289723346ae46ced.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42910f310db6b67881ac5cdc5b6f6d54",
+    "url": "/git-search/index.html"
+  },
+  {
+    "revision": "81a6cd4af893e92c8923",
+    "url": "/git-search/static/css/2.53264b96.chunk.css"
+  },
+  {
+    "revision": "6583e342d259ec85a443",
+    "url": "/git-search/static/css/main.25ca819c.chunk.css"
+  },
+  {
+    "revision": "81a6cd4af893e92c8923",
+    "url": "/git-search/static/js/2.c0c447c8.chunk.js"
+  },
+  {
+    "revision": "6583e342d259ec85a443",
+    "url": "/git-search/static/js/main.ef9ebb37.chunk.js"
+  },
+  {
+    "revision": "7055b829b857593876f0",
+    "url": "/git-search/static/js/runtime~main.32812958.js"
+  },
+  {
+    "revision": "c66b213d1dfff391c61146727032f068",
+    "url": "/git-search/static/media/chevron-down.c66b213d.svg"
+  },
+  {
+    "revision": "90f47098c8908802053427f30191c643",
+    "url": "/git-search/static/media/chevron-up.90f47098.svg"
+  },
+  {
+    "revision": "d3a070848269031d2d69df7a697405a5",
+    "url": "/git-search/static/media/circle.d3a07084.svg"
+  },
+  {
+    "revision": "9a8c65efdbf530e9d73126925fa02ff1",
+    "url": "/git-search/static/media/git-branch.9a8c65ef.svg"
+  },
+  {
+    "revision": "f6cc123478e4498c8855d3b738deb6e2",
+    "url": "/git-search/static/media/git_search.f6cc1234.svg"
+  },
+  {
+    "revision": "38a74b27d96123e0ecf8746080ed3f4a",
+    "url": "/git-search/static/media/git_search_twoliner.38a74b27.svg"
+  },
+  {
+    "revision": "275269c25181d86893b18de50979aea0",
+    "url": "/git-search/static/media/half-circle.275269c2.svg"
+  },
+  {
+    "revision": "5437e5214c32dc0a8b1e3e3749edd831",
+    "url": "/git-search/static/media/search.5437e521.svg"
+  },
+  {
+    "revision": "ac8ea2135e1379656f02ca2eea26cc87",
+    "url": "/git-search/static/media/star.ac8ea213.svg"
+  }
+]);
